11520e13a980fc1a35d174b868c9e0f6eb98008d
Konstantin Sukharev <poed@me.com>
2015-05-13 12:24:23 +0300

    Add OpenEmu-SDK

---
+++ Magikombat/GamepadKit/OpenEmu-SDK-Bridging-Header.h
@@ -0,0 +1,7 @@
+#ifndef Magikombat_OpenEmu_SDK_Bridging_Header_h
+#define Magikombat_OpenEmu_SDK_Bridging_Header_h
+
+#import <OpenEmuBase/OpenEmuBase.h>
+#import <OpenEmuSystem/OpenEmuSystem.h>
+
+#endif
